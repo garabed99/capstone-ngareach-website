a2b46329a3ab09ff7aa3e9677c45feeef9756fef
Garabed Baghsarian <garabed.baghsarian@gmail.com>
2022-03-10 13:54:16 +0400

    feat(backend): first commit folder

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT;
+const mongoPath = process.env.MONGO_PATH;
+console.log("mongoPath: => ", mongoPath);
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+    console.log(`Server is up and listening on ${port} 🚀`);
+  });
