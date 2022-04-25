b5b0f363a1fd7c4d26ae9785919a989e7fe8eaca
Garabed Baghsarian <garabed.baghsarian@gmail.com>
2022-04-25 15:44:00 +0400

    feat(frontend-navbar): added burgerIcon when resizing window

--- frontend/src/components/navbar-section/NavBar.js
+++ frontend/src/components/navbar-section/NavBar.js
@@ -5,22 +5,29 @@ import {
   DialogActions,
   DialogContent,
   DialogTitle,
+  Divider,
+  Hidden,
+  IconButton,
+  SwipeableDrawer,
   Toolbar,
   Typography,
 } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import { Link } from "react-router-dom";
 import "../styles/NavBar.css";
 import { useState } from "react";
 
 export default function NavBar() {
-  const [open, setOpen] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [openDrawer, setOpenDrawer] = useState(false);
 
   function handleClickOpen() {
-    setOpen(true);
+    setOpenDialog(true);
   }
 
   function handleClickClose() {
-    setOpen(false);
+    setOpenDialog(false);
   }
 
   return (
@@ -29,60 +36,72 @@ export default function NavBar() {
         <AppBar position="sticky">
           <Toolbar>
             <div className="logo-container">
-              <Typography variant="h5" style={{color: "black"}}>Նկաreach</Typography>
+              <Typography variant="h5" style={{ color: "black" }}>
+                Նկաreach
+              </Typography>
             </div>
-            <div className="metaData-container">
-              <div className="page-route">
-                <Button>
-                  <Link
-                    to="/home"
-                    style={{ color: "white", textDecoration: "inherit" }}
-                  >
-                    Home
-                  </Link>
-                </Button>
+            <Hidden xsDown>
+              <div className="metaData-container">
+                <div className="page-route">
+                  <Button>
+                    <Link
+                      to="/home"
+                      style={{ color: "white", textDecoration: "inherit" }}
+                    >
+                      Home
+                    </Link>
+                  </Button>
+                </div>
+                <div className="page-route">
+                  <Button>
+                    <Link
+                      to="/howitworks"
+                      style={{ color: "white", textDecoration: "inherit" }}
+                    >
+                      How It Works
+                    </Link>
+                  </Button>
+                </div>
+                <div className="page-route">
+                  <Button>
+                    <Link
+                      to="/reviews"
+                      style={{ color: "white", textDecoration: "inherit" }}
+                    >
+                      Reviews
+                    </Link>
+                  </Button>
+                </div>
+                <div className="page-route">
+                  <Button>
+                    <Link
+                      to="/pricing"
+                      style={{ color: "white", textDecoration: "inherit" }}
+                    >
+                      Pricing
+                    </Link>
+                  </Button>
+                </div>
+                <div className="page-route">
+                  <Button>
+                    <Link
+                      to="/photographerslist"
+                      style={{ color: "white", textDecoration: "inherit" }}
+                    >
+                      Our Photographers
+                    </Link>
+                  </Button>
+                </div>
               </div>
-              <div className="page-route">
-                <Button>
-                  <Link
-                    to="/howitworks"
-                    style={{ color: "white", textDecoration: "inherit" }}
-                  >
-                    How It Works
-                  </Link>
-                </Button>
-              </div>
-              <div className="page-route">
-                <Button>
-                  <Link
-                    to="/reviews"
-                    style={{ color: "white", textDecoration: "inherit" }}
-                  >
-                    Reviews
-                  </Link>
-                </Button>
-              </div>
-              <div className="page-route">
-                <Button>
-                  <Link
-                    to="/pricing"
-                    style={{ color: "white", textDecoration: "inherit" }}
-                  >
-                    Pricing
-                  </Link>
-                </Button>
-              </div>
-              <div className="page-route">
-                <Button>
-                  <Link
-                    to="/photographerslist"
-                    style={{ color: "white", textDecoration: "inherit" }}
-                  >
-                    Our Photographers
-                  </Link>
-                </Button>
-              </div>
-            </div>
+            </Hidden>
+            <Hidden smUp>
+              <IconButton>
+                <MenuIcon
+                  style={{ color: "white" }}
+                  onClick={() => setOpenDrawer(true)}
+                />
+              </IconButton>
+            </Hidden>
 
             <div className="registration-container">
               <Button
@@ -92,7 +111,7 @@ export default function NavBar() {
                 Register
               </Button>
               <Dialog
-                open={open}
+                open={openDialog}
                 onClose={handleClickClose}
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description"
@@ -120,7 +139,7 @@ export default function NavBar() {
                       Photographer
                     </Link>
                   </Button>
-                  <Button onClick={handleClickClose} color="primary" >
+                  <Button onClick={handleClickClose} color="primary">
                     <Link
                       to="/login"
                       style={{ color: "inherit", textDecoration: "inherit" }}
@@ -132,6 +151,60 @@ export default function NavBar() {
               </Dialog>
             </div>
           </Toolbar>
+          <SwipeableDrawer
+            anchor="left"
+            open={openDrawer}
+            onOpen={() => setOpenDrawer(true)}
+            onClose={() => setOpenDrawer(false)}
+          >
+            <div>
+              <IconButton>
+                <ChevronLeftIcon />
+              </IconButton>
+            </div>
+
+            <Divider />
+            <Button>
+              <Link
+                to="/home"
+                style={{ color: "blue", textDecoration: "inherit" }}
+              >
+                Home
+              </Link>
+            </Button>
+            <Button>
+              <Link
+                to="/howitworks"
+                style={{ color: "blue", textDecoration: "inherit" }}
+              >
+                How It Works
+              </Link>
+            </Button>
+            <Button>
+              <Link
+                to="/reviews"
+                style={{ color: "blue", textDecoration: "inherit" }}
+              >
+                Reviews
+              </Link>
+            </Button>
+            <Button>
+              <Link
+                to="/pricing"
+                style={{ color: "blue", textDecoration: "inherit" }}
+              >
+                Pricing
+              </Link>
+            </Button>
+            <Button>
+              <Link
+                to="/photographerslist"
+                style={{ color: "blue", textDecoration: "inherit" }}
+              >
+                Our Photographers
+              </Link>
+            </Button>
+          </SwipeableDrawer>
         </AppBar>
       </div>
     </>
